431159e7511f9dc4eebf93d9b5d78ef9138824da
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-13 23:29:03 +0100

    default start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
 import './App.css';
 // import { useState } from 'react';
-import { FeedbackForm } from './FeedbackForm/FeedbackForm';
 
 export const App = () => {
-  return (
-    <div>
-      <FeedbackForm />
-    </div>
-  );
+  return <div></div>;
 };
