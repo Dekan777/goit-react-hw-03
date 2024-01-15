ff31a939a50461498bacfc51c6e45fb3d1ab89c6
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-01-15 02:13:56 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,35 +1,3 @@
-// import { BsFillPersonFill, BsFillTelephoneFill } from 'react-icons/bs';
-// import css from './ContactList.module.css';
-// import { useId } from 'react';
-
-// export const ContactList = ({ value }) => {
-//   const nameFieldId = useId();
-
-//   const handleDeleteClick = id => {
-//     console.log('id:', id);
-//     // Дополнительные действия при удалении, если необходимо
-//   };
-
-//   return (
-//     <div>
-//       {value.map(({ name, number }, id) => (
-//         <div key={id}>
-//           <p>
-//             <BsFillPersonFill className={css.myIcon} size="20" />
-//             {name}
-//           </p>
-//           <p>
-//             <BsFillTelephoneFill className={css.myIcon} size="16" />
-//             {number}
-//           </p>
-//           <button onClick={() => handleDeleteClick(id)} id={nameFieldId}>
-//             Delete
-//           </button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 import { BsFillPersonFill, BsFillTelephoneFill } from 'react-icons/bs';
 import css from './ContactList.module.css';
 import { useId } from 'react';
